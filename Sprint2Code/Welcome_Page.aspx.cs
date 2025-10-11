b9d4c689f72c12033fb93080fe2c4b83fa83a445
zachkoopman <koopmanzachary@gmail.com>
2025-10-10 19:23:18 -0700

    Add files via upload

---
+++ Sprint2Code/Welcome_Page.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CyberApp_FIA
+{
+    public partial class Welcome_Page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Empty for now, .aspx page handles most functions
+        }
+
+        protected void BtnCreate_Click(object sender, EventArgs e)
+        {
+            //Add Logic Later
+        }
+    }
+}
